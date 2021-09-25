51301a0639541f11b1aa902b472884ef593660cd
akinari <a.mizo0919@gmail.com>
2021-09-25 16:14:38 +0900

    フロントの調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Board.create!(body: 'test', job: 'test', age: '30')
+Board.create!(body: '公園で100円拾ったんで交番に届けました！', job: '自称無職', age: '43')
 
 User.create!(email: "runmizzo@email.com", password: "a.mizo0919@", password_confirmation: "a.mizo0919@")
